89c15f09b219f09862bfccf31678ef7dc5d84dc8
Jorge De Mesa <demesajorge@gmail.com>
2020-03-24 16:56:24 -0400

    end of section 6

--- DatingApp-SPA/src/app/_services/alertify.service.ts
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -7,5 +7,24 @@ import * as alertify from 'alertifyjs';
 export class AlertifyService {
 
 constructor() { }
+  confirm(message: string, okCallBack: () => any) {
+    alertify.confirm(message, (e: any) => {
+      if (e) {
+        okCallBack();
+      } else {}
+    });
+  }
 
+  success(message: string) {
+    alertify.success(message);
+  }
+  error(message: string) {
+    alertify.error(message);
+  }
+  warning(message: string) {
+    alertify.warning(message);
+  }
+  message(message: string) {
+    alertify.message(message);
+  }
 }
